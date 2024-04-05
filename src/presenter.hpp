813956c4efbbb41eaf31f6afc6ece1e9c982b9c3
Severin Brunner <severin.brunner@outlook.de>
2024-04-05 12:37:10 +0200

    Init project with CMake and template source files

---
+++ src/presenter.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <map>
+
+#define PIN_ACTIVE_VAL 1
+#define PIN_USE_PULLUP 0
+
+// Map for pins to keys
+
+// Mode switch; Map pins to new keys (e.g. scroll), not once but continuous press
+
